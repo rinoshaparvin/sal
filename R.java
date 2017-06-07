9fc33c6ed8dba52fb1df3c30d2ba037f2c149996
rinoshaparvin <rinosha14@gmail.com>
2017-06-07 18:17:13 +0530

    Create R.java

---
+++ R.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class R
+{
+public static void main(String args[])
+{
+Scanner S=new Scanner(System.in);
+int n;
+n=S.nextInt();
+if(n%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
